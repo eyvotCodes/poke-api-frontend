d1e9b00392031cb43d888cb696a3bc5a30314f00
Fabián Leyva <fleyva.home@outlook.com>
2025-03-13 10:20:06 -0600

    shadcn/ui setup, components for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import './App.css';
+import { Button } from "@/components/ui/button.jsx";
+
 
 function App() {
 
-  return (
-    <>
-      <div className={ 'text-xl' }>
-        Vite + React + Tailwind
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className={ 'flex flex-col items-center justify-center min-h-svh' }>
+                <span>Vite + React + Tailwind + Shadcn</span>
+                <Button>Click me</Button>
+            </div>
+        </>
+    )
+
 }
+
 
 export default App
