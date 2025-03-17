dffd922fdc835fb64f03d08d301032007aa072bf
Fabián Leyva <fleyva.home@outlook.com>
2025-03-17 04:38:20 -0600

    change development environment to production

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 // dev environment
 export const ENV_DEVELOPMENT = 'development';
 export const ENV_PRODUCTION = 'production';
-export const ENVIRONMENT = ENV_DEVELOPMENT; // update this to change development environment
+export const ENVIRONMENT = ENV_PRODUCTION; // update this to change development environment
 
 // messages
 export const GENERIC_ERROR_LOAD_MESSAGE = 'Oops! Something went wrong. Try refreshing the page.';
